bef915035d1020c218901fa954bced4e40e9da74
Edwin Seclen <jtech3@dbperu.com>
2026-06-14 16:10:53 -0500

    INICIO MENU MODO MOVIL ACTIVO

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Drawer, DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Drawer, DrawerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('huaranchal-web');
+
+  visible4: boolean = false;
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    this.visible4 = !this.visible4;
+  }
 }
