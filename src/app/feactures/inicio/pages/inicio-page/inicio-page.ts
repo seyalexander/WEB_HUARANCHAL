eeeeca7b2f494fb4e1f7a6ebb00cc7eb01c66d84
Edwin Seclen <jtech3@dbperu.com>
2026-06-17 02:11:51 -0500

    MEJORAMIENTO v8

--- src/app/feactures/inicio/pages/inicio-page/inicio-page.ts
+++ src/app/feactures/inicio/pages/inicio-page/inicio-page.ts
@@ -35,4 +35,39 @@ export class InicioPage {
     const section = document.getElementById('descripcionAnime');
     section?.scrollIntoView({ behavior: 'smooth' });
   }
+
+  // ANIMACION CUNADO PASEN EL MOUSE POR ENCIMA DEL HERO
+
+cursorX = 0;
+cursorY = 0;
+mouseX = 0;
+mouseY = 0;
+
+mostrarBrillo = false;
+
+onMouseMove(event: MouseEvent): void {
+
+  // Cursor real
+  this.cursorX = event.clientX;
+  this.cursorY = event.clientY;
+
+  const hero = event.currentTarget as HTMLElement;
+  const rect = hero.getBoundingClientRect();
+
+  // Parallax
+  this.mouseX =
+    (event.clientX / window.innerWidth - 0.5) * 15;
+
+  this.mouseY =
+    (event.clientY / window.innerHeight - 0.5) * 15;
+
+  this.mostrarBrillo = true;
+}
+
+
+
+onMouseLeave(): void {
+
+  this.mostrarBrillo = false;
+}
 }
