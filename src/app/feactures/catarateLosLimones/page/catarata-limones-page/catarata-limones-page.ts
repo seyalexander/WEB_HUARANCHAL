1309888ef01de1a5f721c66ac206fea759b1ad57
Edwin Seclen <jtech3@dbperu.com>
2026-06-16 21:08:09 -0500

    MEJORAMIENTO v5

--- src/app/feactures/catarateLosLimones/page/catarata-limones-page/catarata-limones-page.ts
+++ src/app/feactures/catarateLosLimones/page/catarata-limones-page/catarata-limones-page.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
 
+export type TabLugar =
+  | 'galeria'
+  | 'historia'
+  | 'souvenirs'
+  | 'ubicacion';
+
 @Component({
   selector: 'app-catarata-limones-page',
   imports: [],
   templateUrl: './catarata-limones-page.html',
   styleUrl: './catarata-limones-page.css',
 })
-export class CatarataLimonesPage {}
+export class CatarataLimonesPage {
+   tabSeleccionado: TabLugar = 'galeria';
+
+  tabs = [
+    {
+      id: 'galeria',
+      icono: '📸',
+      nombre: 'Galería'
+    },
+    {
+      id: 'historia',
+      icono: '📖',
+      nombre: 'Historia'
+    },
+    {
+      id: 'souvenirs',
+      icono: '🎁',
+      nombre: 'Souvenirs'
+    },
+    {
+      id: 'ubicacion',
+      icono: '📍',
+      nombre: 'Cómo llegar'
+    }
+  ];
+}
