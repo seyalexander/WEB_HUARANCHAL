41f72eec2f8ae3d32ec19d9db97bbb403822b263
Edwin Seclen <jtech3@dbperu.com>
2026-06-19 14:56:03 -0500

    mejorameinteo apartado lista de lugares turistico modo pc v3

--- src/app/feactures/lugares-turisticos/page/lugares-turisticos-pages/lugares-turisticos-pages.ts
+++ src/app/feactures/lugares-turisticos/page/lugares-turisticos-pages/lugares-turisticos-pages.ts
@@ -7,7 +7,9 @@ export interface lugarTuristico {
   titleH3: string,
   titleH1: string,
   descripcion: string,
-  routerLink: string
+  routerLink: string,
+  fraseTurista?: string, 
+  estadoTurista?:string
 }
 
 @Component({
@@ -22,39 +24,38 @@ export class LugaresTuristicosPages implements OnInit {
 
   urlGlobal: string = "assets"
   carouselDirection: 'left' | 'right' | null = null;
+  estadoTurista?: 'feliz' | 'sorprendido' | 'relajado';
   isAnimating = false;
 
-  items:lugarTuristico[] = [
-    {
-      imagen: `${this.urlGlobal}/CATARATA-IMG.jpeg`,
-      titleH3: "Catarata",
-      titleH1: "Pakcha",
-      descripcion: "Una impresionante caída de agua rodeada de vegetación y senderos naturales.",
-      routerLink: "/CatarataPakcha"
-    },
-    {
-      imagen: `${this.urlGlobal}/LUGARES/BANIOS_TERMALES/TERMALES_01.jpeg`,
-      titleH3: "Baños",
-      titleH1: "Termales",
-      descripcion: "Espacios ideales para relajarse y disfrutar de aguas naturales.",
-      routerLink: "/BaniosTermales"
-    },
-    {
-      imagen: `${this.urlGlobal}/LUGARES/BANIOS_TERMALES/TERMALES_01.jpeg`,
-      titleH3: "Cerro",
-      titleH1: "Chillin",
-      descripcion: "Espacios ideales para relajarse y disfrutar de aguas naturales.",
-      routerLink: "/BaniosTermales"
-    },
-    {
-      imagen: `${this.urlGlobal}/LUGARES/BANIOS_TERMALES/TERMALES_01.jpeg`,
-      titleH3: "Catarata",
-      titleH1: "Las Trillizas",
-      descripcion: "Espacios ideales para relajarse y disfrutar de aguas naturales.",
-      routerLink: "/BaniosTermales"
-    },
-    
-  ]
+  items: lugarTuristico[] = [
+  {
+    imagen: `${this.urlGlobal}/CATARATA-IMG.jpeg`,
+    titleH3: "Catarata",
+    titleH1: "Pakcha",
+    descripcion: "Una impresionante caída de agua rodeada de vegetación y senderos naturales.",
+    routerLink: "/CatarataPakcha",
+    fraseTurista: "¡Wow! Este lugar sí que me deja sin palabras...",
+    estadoTurista: 'sorprendido'
+  },
+  {
+    imagen: `${this.urlGlobal}/LUGARES/BANIOS_TERMALES/TERMALES_01.jpeg`,
+    titleH3: "Baños",
+    titleH1: "Termales",
+    descripcion: "Espacios ideales para relajarse y disfrutar de aguas naturales.",
+    routerLink: "/BaniosTermales",
+    fraseTurista: "Esto sí es vida... relax total 😌",
+    estadoTurista: 'relajado'
+  },
+  {
+    imagen: `${this.urlGlobal}/LUGARES/BANIOS_TERMALES/TERMALES_01.jpeg`,
+    titleH3: "Mirador",
+    titleH1: "Chillin",
+    descripcion: "Espacios ideales para relajarse y disfrutar de aguas naturales.",
+    routerLink: "/BaniosTermales",
+    fraseTurista: "Desde aquí todo el valle se ve increíble...",
+    estadoTurista: 'feliz'
+  }
+]
 
   ngOnInit() {
     this.items;
