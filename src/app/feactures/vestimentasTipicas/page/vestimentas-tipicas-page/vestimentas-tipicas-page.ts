3a8723371bd1de3b48edb3bccf8ee510d3818112
Edwin Seclen <jtech3@dbperu.com>
2026-06-19 00:29:13 -0500

    aprtaod de vestimetnas completo

--- src/app/feactures/vestimentasTipicas/page/vestimentas-tipicas-page/vestimentas-tipicas-page.ts
+++ src/app/feactures/vestimentasTipicas/page/vestimentas-tipicas-page/vestimentas-tipicas-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterLugaresTuristicos } from "../../../../shared/components/footer-lugares-turisticos/footer-lugares-turisticos";
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-vestimentas-tipicas-page',
-  imports: [FooterLugaresTuristicos],
+  imports: [FooterLugaresTuristicos, NgClass, CommonModule],
   templateUrl: './vestimentas-tipicas-page.html',
   styleUrl: './vestimentas-tipicas-page.css',
 })
@@ -30,22 +31,26 @@ export class VestimentasTipicasPage {
     {
       nombre: 'Sombrero',
       descripcion:
-        'Prenda utilizada para protegerse del sol y las condiciones climáticas de la sierra.'
+        'Prenda utilizada para protegerse del sol y las condiciones climáticas de la sierra.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_sombrero.png'
     },
     {
       nombre: 'Poncho',
       descripcion:
-        'Elaborado tradicionalmente en lana. Brinda abrigo y forma parte de la identidad cultural.'
+        'Elaborado tradicionalmente en lana. Brinda abrigo y forma parte de la identidad cultural.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_poncho.png'
     },
     {
       nombre: 'Camisa',
       descripcion:
-        'Prenda de uso diario utilizada durante las labores agrícolas y festividades.'
+        'Prenda de uso diario utilizada durante las labores agrícolas y festividades.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_camisa.png'
     },
     {
       nombre: 'Pantalón',
       descripcion:
-        'Generalmente confeccionado para facilitar el trabajo en el campo.'
+        'Generalmente confeccionado para facilitar el trabajo en el campo.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_pantalon.png'
     }
   ];
 
@@ -53,22 +58,26 @@ export class VestimentasTipicasPage {
     {
       nombre: 'Sombrero',
       descripcion:
-        'Complemento tradicional utilizado en actividades cotidianas y festividades.'
+        'Complemento tradicional utilizado en actividades cotidianas y festividades.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_sombrero.png'
     },
     {
-      nombre: 'Manta',
+      nombre: 'Rebozo',
       descripcion:
-        'Tejida artesanalmente y utilizada para protegerse del frío o transportar objetos.'
+        'Tejida artesanalmente y utilizada para protegerse del frío o transportar objetos.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_rebozo.png'
     },
     {
       nombre: 'Pollera',
       descripcion:
-        'Falda tradicional de gran importancia dentro de la vestimenta femenina andina.'
+        'Falda tradicional de gran importancia dentro de la vestimenta femenina andina.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_pollera.png'
     },
     {
       nombre: 'Tejidos',
       descripcion:
-        'Accesorios elaborados con técnicas heredadas por generaciones.'
+        'Accesorios elaborados con técnicas heredadas por generaciones.',
+        imagen: 'assets/VESTIMENTAS/vestimenta_tejido.png'
     }
   ];
 
@@ -89,4 +98,6 @@ export class VestimentasTipicasPage {
   seleccionarPrenda(prenda: any) {
     this.prendaSeleccionada = prenda;
   }
+
+  
 }
