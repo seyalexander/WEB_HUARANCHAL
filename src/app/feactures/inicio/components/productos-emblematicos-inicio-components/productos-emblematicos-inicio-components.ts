0f54277ebedc146b76ab85b311bb2ac7eb8dba75
Edwin Seclen <jtech3@dbperu.com>
2026-09-03 00:49:01 -0500

    Personaje en los productos emblemáticos

--- src/app/feactures/inicio/components/productos-emblematicos-inicio-components/productos-emblematicos-inicio-components.ts
+++ src/app/feactures/inicio/components/productos-emblematicos-inicio-components/productos-emblematicos-inicio-components.ts
@@ -5,6 +5,7 @@ interface Producto {
     nombre: string;
     descripcion: string;
     imagen: string;
+    personaje: string;
     animacion: string;
 }
 
@@ -17,30 +18,40 @@ interface Producto {
 export class ProductosEmblematicosInicioComponents {
 
     productos: Producto[] = [
+
         {
             nombre: 'Café',
             descripcion: 'Uno de los productos más representativos del distrito.',
             imagen: 'assets/CAFE-IMG.jpg',
+            personaje: 'assets/INICIO/PRODUCTOS/personaje-cafe.png',
             animacion: 'slide-in-from-l-20'
         },
+
         {
             nombre: 'Naranja',
             descripcion: 'Fruta emblemática del distrito, apreciada por su dulzura y frescura.',
             imagen: 'assets/NARANJA-IMG.jpg',
+            personaje: 'assets/INICIO/PRODUCTOS/personaje-naranja.png',
             animacion: 'slide-in-from-l-20'
         },
+
         {
             nombre: 'Granadilla',
             descripcion: 'Conocida por su sabor delicado y propiedades nutritivas.',
             imagen: 'assets/GRANADILLAS-IMG.avif',
+            personaje: 'assets/INICIO/PRODUCTOS/personaje-cafe.png',
             animacion: 'slide-in-from-r-20'
         },
+
         {
             nombre: 'Lima',
             descripcion: 'Producto tradicional utilizado en la gastronomía regional.',
             imagen: 'assets/LIMAS-IMG.jpg',
+            personaje: 'assets/INICIO/PRODUCTOS/personaje-naranja.png',
             animacion: 'slide-in-from-r-20'
         }
+
     ];
+
 
 }
