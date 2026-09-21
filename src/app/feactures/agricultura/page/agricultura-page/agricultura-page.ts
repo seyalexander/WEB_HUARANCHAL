e4a0d38ccdd5c6a3dce78ec3e142fdbc34035343
Edwin Seclen <jtech3@dbperu.com>
2026-09-21 13:35:34 -0500

    Mejoramiento de imagenes a webp de agreicultura

--- src/app/feactures/agricultura/page/agricultura-page/agricultura-page.ts
+++ src/app/feactures/agricultura/page/agricultura-page/agricultura-page.ts
@@ -11,28 +11,28 @@ export class AgriculturaPage {
     {
       id: 1,
       nombre: 'Granadilla',
-      imagen: 'assets/AGRICULTURA/granadilla_01.png',
+      imagen: 'assets/AGRICULTURA/granadilla_01.webp',
       modelo: 'assets/AGRICULTURA/agricultor_maiz_01.png',
       descripcion: 'Cultivo fundamental en la alimentación y economía local.'
     },
     {
       id: 2,
       nombre: 'Papa',
-      imagen: 'assets/AGRICULTURA/papa_01.png',
+      imagen: 'assets/AGRICULTURA/papa_01.webp',
       modelo: 'assets/AGRICULTURA/agricultor_papa_01.png',
       descripcion: 'Producto tradicional cultivado en las zonas altas.'
     },
     {
       id: 3,
       nombre: 'Naranjas',
-      imagen: 'assets/AGRICULTURA/naranja_01.png',
+      imagen: 'assets/AGRICULTURA/naranja_01.webp',
       modelo: 'assets/AGRICULTURA/agricultor_trigo_01.png',
       descripcion: 'Utilizado en la elaboración de diversos alimentos.'
     },
     {
       id: 4,
       nombre: 'Café',
-      imagen: 'assets/AGRICULTURA/cafe_01.png',
+      imagen: 'assets/AGRICULTURA/cafe_01.webp',
       modelo: 'assets/AGRICULTURA/agricultor_cebada_01.png',
       descripcion: 'Cultivo resistente adaptado al clima serrano.'
     }
