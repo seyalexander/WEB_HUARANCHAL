c65178f4b604b42f0d23b50fb574e0f02a51e942
Edwin Seclen <jtech3@dbperu.com>
2026-09-21 15:02:05 -0500

    Mejoramiento de apartado de caceríos en INICIO

--- src/app/feactures/inicio/components/caserios-component/caserios-component.ts
+++ src/app/feactures/inicio/components/caserios-component/caserios-component.ts
@@ -1,56 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 import { Caserio } from '../../../../shared/data/Inicio/Inicio-Caserios.interface';
 import { caseriosHuaranchal } from '../../../../shared/data/Inicio/Inicio-Caserios.data';
-
 
 @Component({
     selector: 'app-caserios-component',
     standalone: true,
     imports: [
         CommonModule,
-        FormsModule
+        RouterLink
     ],
     templateUrl: './caserios-component.html',
     styleUrl: './caserios-component.css',
 })
 export class CaseriosComponent {
 
-    busqueda: string = '';
-
     caserios: Caserio[] = caseriosHuaranchal;
 
-    caserioSeleccionado: Caserio =
-        this.caserios[0];
+    filaSuperior: Caserio[] = this.caserios.slice(0, 11);
 
-
-    get caseriosFiltrados(): Caserio[] {
-
-        const texto =
-            this.busqueda
-                .trim()
-                .toLowerCase();
-
-        if (!texto) {
-            return this.caserios;
-        }
-
-        return this.caserios.filter(
-            caserio =>
-                caserio.nombre
-                    .toLowerCase()
-                    .includes(texto)
-        );
-
-    }
-
-
-    seleccionar(caserio: Caserio): void {
-
-        this.caserioSeleccionado =
-            caserio;
-
-    }
+    filaInferior: Caserio[] = this.caserios.slice(11);
 
 }
